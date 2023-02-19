2c90692eafc51008ec893c6b95365a3257a5535f
WNWolter <113820891+WINWOlter@users.noreply.github.com>
2023-02-19 09:21:12 +0000

    я добавив старіння до дерева

--- main.py
+++ main.py
@@ -3,6 +3,20 @@ class Tree:
   height = 600
   color = "brown"
 
+  def __init__(self):
+     print("")
+
+  def get_older(self):
+      self.age += 1
+
+ 
+my_tree = Tree()
+
+print(my_tree.age)
+
+my_tree.get_older()
+
+   
 
   
   
