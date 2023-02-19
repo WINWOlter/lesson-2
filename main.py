396bb5a6516bdff0fd738deb18e907f7e0a7f44b
WNWolter <113820891+WINWOlter@users.noreply.github.com>
2023-02-19 09:33:14 +0000

    добавив назву та породу дерева

--- main.py
+++ main.py
@@ -2,19 +2,25 @@ class Tree:
   age = 200
   height = 600
   color = "brown"
+  leafs = 1000000
 
-  def __init__(self):
-     print("")
+  def __init__(self, type):
+     self.type = type
 
   def get_older(self):
       self.age += 1
+    
+
+  
 
  
-my_tree = Tree()
+my_tree = Tree("Akacia")
 
 print(my_tree.age)
 
 my_tree.get_older()
+
+
 
    
 
